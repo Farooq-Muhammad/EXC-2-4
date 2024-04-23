4095cb4e0abadb5c97d80c9f889c88a1e18e5223
Farooq-Muhammad <167761676+Farooq-Muhammad@users.noreply.github.com>
2024-04-23 19:20:36 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Question 1
+console.log("helloe world");
+//QUESTION 2
+var personName = ("Aric");
+console.log("hellow", personName, "Would you like to learn some python langauge");
+//QUESTION NO 3"
+var PersonName = "farooq";
+console.log("lowercase:", PersonName.toLocaleLowerCase());
+console.log("uppercase:", PersonName.toUpperCase());
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toLocaleUpperCase(); }));
+// Question no 4
+var quote = "A person who made a mistake never tried anything new";
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, ").concat(quote);
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,16 @@
+// Question 1
+console.log("helloe world");
+//QUESTION 2
+let personName: string = ("Aric")
+console.log("hellow",personName,"Would you like to learn some python langauge");
+//QUESTION NO 3"
+let PersonName:string = "farooq"
+console.log("lowercase:",PersonName. toLocaleLowerCase());
+console.log("uppercase:",PersonName.toUpperCase());
+console.log("titlecase:",personName.replace(/\bw/g,c => c.toLocaleUpperCase()));
+
+// Question no 4
+let quote:string ="A person who made a mistake never tried anything new";
+let famous_person:string = "Albert Einstein";
+let message:string= `${famous_person} once said, ${quote}`;
+console.log(message);
